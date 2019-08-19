39d3fdacfb0dc1a5847ecb753f039017077c4d6b
Ubuntu <ubuntu@ip-172-31-43-207.us-east-2.compute.internal>
2019-08-19 15:43:40 +0000

    assessment 3, having troubles with #7

--- js-intermediate.js
+++ js-intermediate.js
@@ -4,17 +4,25 @@
 // Be sure to use all given test cases
 
 // 1. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-
-
+let getFib = (num) =>{
+  let arr = [0, 1]
+  for(let i=2; i<num; i++){
+    arr.push(arr[i-1] + arr[i-2])
+  } return arr
+}
+console.log(getFib(11))
 // 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
 
 var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [9, 7, 199, -9]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [7, 23, -823]
+var num = [0, 1, 2, 3, 4]
+let oddChecker = (arr) =>{
+  return arr.filter(value=> value%2 === 1 && typeof value === "number")
+}
 
-
+console.log(oddChecker(fullArr1))
 
 // 3. Given the object below, complete the console.log to find specific information:
 
@@ -29,13 +37,13 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 
 // 4. Write a function that takes one argument of type array and returns the contents of that array in reverse order.
@@ -45,41 +53,83 @@ var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
 var originalArray2 = ["a", "t", "l", "e", "d"]
 // Expected output: ["d", "e", "l", "t", "a"]
 
+let reverse = (arr) =>{
+  return arr.reverse('')
+}
 
-
+console.log(reverse(originalArray1))
 // 5. Write the code that would make this test pass.
 
-describe("multTwo", () => {
-    test ("returns an array with all the numbers multiplied by two", () => {
-        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
-        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
-        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
-    })
-})
+// describe("multTwo", () => {
+//     test ("returns an array with all the numbers multiplied by two", () => {
+//         expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+//         expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+//         expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+//     })
+// })
 
+let multTwo = (arr) =>{
+  let newArr = arr.map(value => value * 2)
+return newArr
+}
 
+// console.log(multTwo(arr))
 
 // 6. Write a function called letterCounter that logs the number of times the letter "l" appears in ourString.
 
 var ourString = "Hello Learn Students!"
 // Expected output: 3
 
+let letterCounter = (str) =>{
+  let arr = str.toLowerCase().split('')
+  letterCount = 0
+  let letter = "l"
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].includes(letter))
+    letterCount += 1
+    }return letterCount
+  }
 
+console.log(letterCounter(ourString))
 
 // 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
 
 var middleLetters1 = "hello"
 // Expected output: “l”
-var middleLetters2 = "lamas"
+var middleLetters2 = "llamas"
 // Expected output: “am”
 
-
-
+let middleLetter = (str) =>{
+  let i=0
+  let arr = str.split('')
+  for(i=0; i<arr.length / 2; i++){
+  }if(str%2 !== 0){
+    return arr[i-1]
+  }else
+    return arr[i-1] + arr[i]
+}
+console.log(middleLetter(middleLetters2))
 
 // 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
+class Sphere{
+  constructor(radius){
+    this.radius = radius
+  }
+  get area(){
+    return this.calcArea()
+  }
+  calcArea(){
+    return 4*Math.PI*Math.pow(this.radius, 2)
+  }
+}
 
-
+let small = new Sphere(2)
+let medium = new Sphere (4)
+let large = new Sphere (6)
+console.log(small.area)
+console.log(medium.area)
+console.log(large.area)
 
 // 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
 // Don't overthink this one, it's just destrcturing practice.
@@ -93,13 +143,26 @@ var myCar = {
   }
 }
 
+const { doors, transmission } = myCar.specs
 
+console.log(doors, transmission)
 
 // 10. Stretch: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
-var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
-var numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
-var numbersToAdd3 = []
-// Expected output: []
+// var numbersToAdd1 = [2, 4, 45, 9]
+// // Excpected output: [2, 6, 51, 60]
+// var numbersToAdd2 = [0, 7, -8, 12]
+// // Expected output: [0, 7, -1, 11]
+// var numbersToAdd3 = []
+// // Expected output: []
+
+// let accumulating = (num) =>{
+//   let i=0
+//   new arr4 = []
+//   let newArr = num[i]
+//   let newArr1 = newArr + num[i+1]
+//   let newArr2 = newArr1 + num[i+2]
+//   let newArr3 = newArr2 + num[i+3]
+//   return 
+// }
+// console.log(accumulating(numbersToAdd2))
--- js-intermediate.md
+++ js-intermediate.md
@@ -5,36 +5,36 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What are the HTTP verbs? Which CRUD action corresponds to each verb?
 
-  Your answer:
+  Your answer: get, post, puts, patch, and delete. Create is post, read is get, update is puts and patch, delete is delete.
 
   Researched answer:
 
 
 2. Why would you use object destructuring?
 
-  Your answer:
+  Your answer: You would use it to make it faster to call an value in an object.
 
-  Researched answer:
-
+  Researched answer: The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
 
 
 3. Why is testing important?
 
-  Your answer:
+  Your answer: testing allows for you to see if you could create any errors and make sure it runs correctly
 
-  Researched answer:
+  Researched answer: Each new commit you make on a project can break an existing function so testing helps prevent you from pushing any code that could harm existing code.
 
 
 4. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: a class is a bluprint for objects. 
+ 
+  Researched answer: The difference is simple and conceptual. A class is a template for objects. A class defines object properties including a valid range of values, and a default value. A class also describes object behavior. An object is a member or an "instance" of a class. An object has a state in which all of its properties have values that you either explicitly define or that are defined by default settings.
 
-  Researched answer:
 
 
 5. What did you learn during the group project this week? Please include any additional feedback you may have.
 
-  Your answer:
+  Your answer: How to pass stuff from a parent to a child and back to a parent component. 
 
 
 
@@ -42,57 +42,57 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. How do you link a CSS file to your HTML page?
 
-  Your answer:
+  Your answer: by typing <link> in your html page
 
-  Researched answer:
+  Researched answer: You can link this external file (.css file) to your HTML document file using the < link > tag 
 
 
 2. What is the difference between a div and a span?
 
   Your answer:
 
-  Researched answer:
+  Researched answer: div is a block element and span is inline. divs are used for sections of documents while spans are used for small portions of text or images.
 
 
 3. What is a CSS class? When should you use an id instead of a class?
 
-  Your answer:
+  Your answer: a css class is what you use to for object layout and design. and you use an id over a class for unique features
 
-  Researched answer:
+  Researched answer: Classes have no special abilities in the browser, but ID's do have one very important trick up their sleeve. This is the "hash value" in the URL.
 
 
 4. Name 4 semantic HTML tags.
 
-  Your answer:
+  Your answer: <nav> <header> <footer> <table>
 
-  Researched answer:
+  Researched answer: <nav> <header> <footer> <table>
 
 
 5. What are three options for creating responsive design?
 
-  Your answer:
+  Your answer: flexible layouts
 
-  Researched answer:
+  Researched answer: flexible layouts, media queries, and flexible media
 
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. What is front end development? Can you identify any tools/skills that are uniquely required of front end developers?
 
-  Your answer:
+  Your answer: Front end development, is development for the users. Experience with html/css.
 
-  Researched answer:
+  Researched answer: To sum it up, front end developers code websites using the building blocks of HTML, CSS, and JavaScript (more on this shortly). They’re the ones who take a design and develop it into a website that works.
 
 
 2. What is block scope in JavaScript?
 
-  Your answer:
+  Your answer: block scope is the scope in which a function can call certain variables
 
-  Researched answer:
+  Researched answer: A block scope is the area within if, switch conditions or for and while loops. Generally speaking, whenever you see {curly brackets}, it is a block. In ES6, const and let keywords allow developers to declare variables in the block scope, which means those variables exist only within the corresponding block.
 
 
 3. How would you explain the idea of "inheritance" in object oriented programming?
 
-  Your answer:
+  Your answer: allows you to call on other objects
 
-  Researched answer:
+  Researched answer: In object-oriented programming, inheritance enables new objects to take on the properties of existing objects. 
